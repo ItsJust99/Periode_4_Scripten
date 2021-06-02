7593e819b32b3425c6c3abb0b977ee016c15bb64
ItsJust99 <sa.van.der.heijden@sintlucasedu.nl>
2021-06-02 11:46:04 +0200

    oof
    it's something

---
+++ Scripten_7/Week_1._3_Assignment/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class Student
+        {
+            // 01. Create a Student.cs class
+            /*
+             * The class has fields and properties for :
+             *   - Name (string)
+             *   - Year of birth (int)
+             *   - Hobby (string)
+             *   - Alive (bool)
+             */
+            private string name;
+            private int yearOfBirth;
+            private string hobby;
+            private bool alive;
+            /*
+         * The class has GETTERS for : 
+         *   - Name
+         *   - Year of birth
+         */
+            public string Name
+            {
+                get { return this.name; }
+            }
+
+            public int YearOfBirth
+            {
+                get { return this.yearOfBirth; }
+            }
+
+            /*
+             * The class has GETTERS and SETTERS for : 
+             *   - Hobby
+             *   - Alive
+             */
+            public string Hobby
+            {
+                get { return this.hobby; }
+                set { this.hobby = value; }
+            }
+
+            public bool Alive
+            {
+                get { return this.alive; }
+                set { this.alive = value; }
+            }
+
+            /*
+             * The class has a default and custom Constructor 
+             */
+            // default constructor
+            public Student()
+            {
+                name = "John Doe";
+                yearOfBirth = 2003;
+                hobby = "None";
+                alive = true;
+            }
+
+            // custom constructor
+            public Student(string name, int yearOfBirth, string hobby, bool alive)
+            {
+                this.name = name;
+                this.yearOfBirth = yearOfBirth;
+                this.hobby = hobby;
+                this.alive = alive;
+            }
+
+            /*
+             * The class has a method which : 
+             *   - Checks if the student is alive and returns the correct string
+             *   - "alive" 
+             *   - or "not alive"
+             */
+            // class method
+            public string isAlive()
+            {
+                if (this.alive == true)
+                {
+                    return "alive";
+                }
+                else
+                {
+                    return "not alive";
+                }
+            }
+        }
+        }
+    }
+}
---
+++ Scripten_7/Week_2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Week_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              *  Assignment:
+              *  
+              *  PREPARATION
+              *  - Read the assignment and make a plan
+              *    What do your classes need?   
+              *    How are you going to tackle the individual parts?   
+              *    Try to create a simple class diagram to describe what your class needs to have
+              *    (you can find an example in the Week_2.1_OOP_Revisited project folder)
+              *    
+              *  CREATE A HERO CLASS
+              *  - The hero has the following attributes:
+              *     - Name : every hero needs a name
+              *     - Hit points : when there are no hit points left the hero is incapacitated
+              *     - Attack bonus 
+              *  - The hero can do the following:
+              *     - Attack : the hero does 1D12 damage + the attack bonus
+              *     - Defend : when the hero defends it takes only half damage
+              *  
+              *  CREATE A MONSTER CLASS
+              *  - The monster has the following attributes:
+              *     - Type : what kind of monster is it
+              *     - Hit points : when there are no hit points left the hero is incapacitated
+              *     - Chance to Flee : the monster has a chance to flee when hit points are below 50%
+              *  - The hero can do the following:
+              *     - Attack : the hero does 2D6 damage
+              *     - Flee : when hit points are below 50% the monster has a chance to flee on it's turn
+              *              when the monster flees the hero wins
+              *              
+              *  LET THE FIGHTING BEGIN!
+              *  - Prepare for the final assignment next week
+              *     - Practice using the Hero and Monster classes
+              *       - Create default and custom objects
+              *       - Use getters
+              *       - Use setters
+              *       - Use methods
+              *     - Create a basic game that lets a hero fight a monster
+             */
+
+            class Hero{
+                string name;
+                bool Hit point;
+                bool attack bonus;
+            }
+            
+            class Monster
+            {
+                int type;
+                bool Hit points;
+                int flee;
+            }
+        }
+    }
+}
